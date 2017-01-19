f0b4ed55088d2eabcc2efad95d27d7445ab44ef5
eugene6472 <eugene6472@gmail.com>
2017-01-19 15:43:54 -0800

    Update exercise.R

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,6 +1,54 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
 
+data("USPersonalExpenditure")
+
+# The variable USPersonalExpenditure is now accessible to you. Unfortunately,
+# it's not a data frame (it's actually what is called a matrix)
+# Test this using the `is.data.frame()` function
+
+is.data.frame(USPersonalExpenditure)
+
+# Luckily, you can simply pass the USPersonalExpenditure variable as an argument
+# to the `data.frame()` function to convert it a data farm. Do this, storing the
+# result in a new variable
+
+new.data <- data.frame(USPersonalExpenditure)
+View(new.data)
+
+# What are the column names of your dataframe?
+colnames(new.data)
+
+
+# Why are they so strange? Think about whether you could use a number like 1940
+# with dollar notation!
+
+new.data$X.USPersonalExpenditure
+
+# What are the row names of your dataframe?
+
+roms(USPersonal)
+
+# Create a column "category" that is equal to your rownames
+
+
+# How much money was spent on personal care in 1940?
+
+
+# How much money was spent on Food and Tobacco in 1960?
+
+
+# What was the highest expenditure category in 1960?
+# Hint: use the `max()` function to find the largest, then compare that to the column
+
+
+# Define a function `DetectHighest` that takes in a year as a parameter, and
+# returns the highest spending category of that year
+
+
+# Using your function, determine the highest spending category of each year# Load R's "USPersonalExpenditure" dataset using the "data()" function
+# This will produce a data frame called `USPersonalExpenditure`
+
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
